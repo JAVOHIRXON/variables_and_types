e51091da27f003e3c9adc82cd41fbc8f964d224d
JAVOHIRXON <kamongaron.j@gmail.com>
2023-09-14 16:17:08 +0500

    bo'ldim

--- begin09.py
+++ begin09.py
@@ -5,8 +5,8 @@ b = 3
 # Assign 2 (int value) to 'c' variable;
 c = 2
 # Assign 4 (int value) to 'd' variable;
-x = 4
+d = 4
 # Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin09' to variable 'k';
-k = a+b-c*x
+k = a+b-c*d
 # # Print the variable 'k'.
 print(k)
